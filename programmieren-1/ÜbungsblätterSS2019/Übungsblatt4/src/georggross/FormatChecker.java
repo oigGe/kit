c215f8a205353fe20e02a039781d8a937e34ad31
Georg Gross <georg.gross@49nord.de>
2019-06-19 21:08:19 +0200

    uebungsblatt 4 fixed regex

--- programmieren-1/ÜbungsblätterSS2019/Übungsblatt4/src/georggross/FormatChecker.java
+++ programmieren-1/ÜbungsblätterSS2019/Übungsblatt4/src/georggross/FormatChecker.java
@@ -14,18 +14,18 @@ public class FormatChecker {
     private static final String START_REGEX_7 = "start ([\\d]+,){9}\\d+;([\\d]+,){9}\\d+";
 
     private static final String ROLL_REGEX_5 = "roll [1-6]";
-
-    private static final String ROLL_REGEX_7 = "roll [1-10]";
+//might need check
+    private static final String ROLL_REGEX_7 = "roll [\\d]+";
 
     private static final String QUIT_REGEX = "quit";
 
     private static final String MOVE_REGEX_STANDARD_5 = "move [1-6],[0-4];[0-4]";
 
-    private static final String MOVE_REGEX_STANDARD_7 = "move [1-10],[0-6];[0-6]";
+    private static final String MOVE_REGEX_STANDARD_7 = "move [\\d]+,[0-6];[0-6]";
 
     private static final String MOVE_REGEX_TORUS_5 = "move [1-6],[-]*[\\d]+;[-]*[\\d]+";
 
-    private static final String MOVE_REGEX_TORUS_7 = "move [1-10],[-]*[\\d]+;[-]*[\\d]+";
+    private static final String MOVE_REGEX_TORUS_7 = "move [\\d]+,[-]*[\\d]+;[-]*[\\d]+";
 
     private static final String PRINT_CELL_5 = "print-cell [0-4];[0-4]";
 
